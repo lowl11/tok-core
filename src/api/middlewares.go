2bf0b937c27984b43ddc622438ef7f85b90c8631
lazy_owl <slimsly11@gmail.com>
2022-12-22 15:08:32 +0600

    move ip address middleware to common

--- src/api/middlewares.go
+++ src/api/middlewares.go
@@ -7,19 +7,17 @@ import (
 
 // setMiddlewares общие миддлы вне зависимости от эндпоинта
 func setMiddlewares(server *echo.Echo) {
-	//
+	server.Use(middlewares.IpAddress)
 }
 
 // setMiddlewaresPublic миддлы для публичных методов
 func setMiddlewaresPublic(group *echo.Group) {
 	group.Use(middlewares.Token)
 	group.Use(middlewares.Session)
-	group.Use(middlewares.IpAddress)
 }
 
 // setMiddlewaresPublicPart миддлы для публичных методов, но для запросов с частичными данными
 func setMiddlewaresPublicPart(group *echo.Group) {
 	group.Use(middlewares.TokenPart)
 	group.Use(middlewares.SessionPart)
-	group.Use(middlewares.IpAddress)
 }
