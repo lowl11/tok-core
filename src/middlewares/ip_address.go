4be9db28ccb63038a08160b84ff2d42cd94af3f8
lazy_owl <slimsly11@gmail.com>
2023-01-07 16:52:19 +0600

    remove ip addresses

--- src/middlewares/ip_address.go
+++ src/middlewares/ip_address.go
@@ -1,8 +1,6 @@
 package middlewares
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,24 +8,6 @@ func IpAddress(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
 		// чтение и проверка ip адреса
 		ipAddress := ctx.RealIP()
-
-		fmt.Println("remote:", ctx.Request().RemoteAddr)
-		fmt.Println("x forwarded for:", ctx.Request().Header.Get("X-Forwarded-For"))
-		fmt.Println("real ip:", ctx.Request().Header.Get("X-Real-Ip"))
-		fmt.Println("")
-
-		requestInJson, err := json.Marshal(ctx.Request())
-		if err != nil {
-			fmt.Println("parse request to json err:", err)
-		}
-
-		fmt.Printf("Request in JSON: %s\n", requestInJson)
-
-		//if strings.Contains(ipAddress, "127.0.0.1") ||
-		//	strings.Contains(ipAddress, "localhost") ||
-		//	strings.Contains(ipAddress, "::1") {
-		//	ipAddress = ctx.Request().RemoteAddr
-		//}
 
 		ctx.Set("ip_address", ipAddress)
 
