401e4d53cc2fcd997baa6113f499f2115be958cf
lazy_owl <slimsly11@gmail.com>
2023-01-13 17:11:08 +0600

    create feeds at night

--- src/job/job_server/internal.go
+++ src/job/job_server/internal.go
@@ -8,21 +8,21 @@ import (
 func (server *Server) tasks() {
 	logger := definition.Logger
 
-	//if _, err := server.scheduler.Cron("55 23 * * *").DoWithJobDetails(server.post.FillUnauthorizedJob); err != nil {
-	//	logger.Error(err, "Fill unauthorized feed job error", layers.Job)
-	//}
-
-	if _, err := server.scheduler.Every(1).Hours().Do(server.post.FillUnauthorizedJob); err != nil {
+	if _, err := server.scheduler.Cron("55 23 * * *").DoWithJobDetails(server.post.FillUnauthorizedJob); err != nil {
 		logger.Error(err, "Fill unauthorized feed job error", layers.Job)
 	}
 
-	//if _, err := server.scheduler.Cron("55 23 * * *").DoWithJobDetails(server.post.FillExploreJob); err != nil {
-	//	logger.Error(err, "Fill explore feed job error", layers.Job)
+	//if _, err := server.scheduler.Every(1).Hours().Do(server.post.FillUnauthorizedJob); err != nil {
+	//	logger.Error(err, "Fill unauthorized feed job error", layers.Job)
 	//}
 
-	if _, err := server.scheduler.Every(1).Hours().Do(server.post.FillExploreJob); err != nil {
+	if _, err := server.scheduler.Cron("55 23 * * *").DoWithJobDetails(server.post.FillExploreJob); err != nil {
 		logger.Error(err, "Fill explore feed job error", layers.Job)
 	}
+
+	//if _, err := server.scheduler.Every(1).Hours().Do(server.post.FillExploreJob); err != nil {
+	//	logger.Error(err, "Fill explore feed job error", layers.Job)
+	//}
 }
 
 func (server *Server) unauthorizedFeed() error {
