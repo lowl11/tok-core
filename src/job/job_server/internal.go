253ab0c3020bb6075d3f0f821a78b36740d36177
lazy_owl <slimsly11@gmail.com>
2023-01-13 18:38:32 +0600

    remove localhost run jobs

--- src/job/job_server/internal.go
+++ src/job/job_server/internal.go
@@ -13,18 +13,18 @@ func (server *Server) tasks() {
 	}
 
 	// !!! uncomment next lines for localhost
-	if _, err := server.scheduler.Every(1).Hours().Do(server.post.FillUnauthorizedJob); err != nil {
-		logger.Error(err, "Fill unauthorized feed job error", layers.Job)
-	}
+	//if _, err := server.scheduler.Every(1).Hours().Do(server.post.FillUnauthorizedJob); err != nil {
+	//	logger.Error(err, "Fill unauthorized feed job error", layers.Job)
+	//}
 
 	if _, err := server.scheduler.Cron("55 23 * * *").Do(server.post.FillExploreJob); err != nil {
 		logger.Error(err, "Fill explore feed job error", layers.Job)
 	}
 
 	// !!! uncomment next lines for localhost
-	if _, err := server.scheduler.Every(1).Hours().Do(server.post.FillExploreJob); err != nil {
-		logger.Error(err, "Fill explore feed job error", layers.Job)
-	}
+	//if _, err := server.scheduler.Every(1).Hours().Do(server.post.FillExploreJob); err != nil {
+	//	logger.Error(err, "Fill explore feed job error", layers.Job)
+	//}
 }
 
 func (server *Server) unauthorizedFeed() error {
