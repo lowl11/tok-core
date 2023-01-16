5fb7b5666be197e67e5a302520e734d3a7f5da65
lazy_owl <slimsly11@gmail.com>
2023-01-17 00:00:46 +0600

    something like fix

--- src/events/notification_event/api.go
+++ src/events/notification_event/api.go
@@ -4,7 +4,6 @@ import (
 	"github.com/google/uuid"
 	"time"
 	"tok-core/src/data/entities"
-	"tok-core/src/data/interfaces"
 	"tok-core/src/repositories/notification_repository"
 )
 
@@ -12,7 +11,7 @@ func (event *Event) SetRepo(notification *notification_repository.Repository) {
 	event.notification = notification
 }
 
-func (event *Event) Push(code, username string, action interfaces.INotificationAction) error {
+func (event *Event) Push(code, username string, action any) error {
 	actionKey := uuid.New().String()
 
 	if err := event.notification.AddItemExist(username, &entities.NotificationAction{
@@ -20,7 +19,7 @@ func (event *Event) Push(code, username string, action interfaces.INotificationA
 		Status:     "new",
 		ActionKey:  actionKey,
 		ActionCode: code,
-		ActionBody: action.Body(),
+		ActionBody: action,
 		CreatedAt:  time.Now(),
 	}); err != nil {
 		return err
