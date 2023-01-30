f46fe0aeed400dfb85ea6fa4cec5102447332849
lazy_owl <slimsly11@gmail.com>
2023-01-30 20:09:50 +0600

    unauthorized user fix

--- src/events/client_session_event/internal.go
+++ src/events/client_session_event/internal.go
@@ -1,19 +1,20 @@
 package client_session_event
 
 import (
+	"strings"
 	"tok-core/src/data/entities"
 )
 
 const (
 	sessionPrefix = "cs_"
 
-	CommonUsername = "u2lLaHKW"
+	CommonUsername = "u2llahkw"
 	commonPassword = "fs21xmiqddddmcm1-vz2w3tvdqfa3r4no5qrc8_o6ku="
 	commonToken    = "1a46181f-7f8b-4498-b230-27ac5ff7a288"
 )
 
 func (event *Event) getMockSession(token, username string) *entities.ClientSession {
-	if token == commonToken && username == CommonUsername {
+	if token == commonToken && strings.ToLower(username) == CommonUsername {
 		return &entities.ClientSession{
 			ID:       777,
 			Username: CommonUsername,
