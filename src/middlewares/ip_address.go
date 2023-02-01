efa1017ee023d209157af1d3226a7cb47d0d3d10
lazy_owl <slimsly11@gmail.com>
2023-02-01 20:07:42 +0600

    turn on middleware log

--- src/middlewares/ip_address.go
+++ src/middlewares/ip_address.go
@@ -2,12 +2,14 @@ package middlewares
 
 import (
 	"github.com/labstack/echo/v4"
+	"tok-core/src/definition"
 )
 
 func IpAddress(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
 		// чтение и проверка ip адреса
 		ipAddress := ctx.RealIP()
+		definition.Logger.Warn("IP address from middleware: " + ipAddress)
 
 		ctx.Set("ip_address", ipAddress)
 
